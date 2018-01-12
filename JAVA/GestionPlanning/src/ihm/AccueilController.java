6d01d1ee27e886b006e50a78c5e901d927543d88
Jean-Baptiste Loutfalla <jb.loutfalla@orange.fr>
2018-01-12 08:12:02 +0100

    Enregistrement de quelques modifs + modification du gitignore

---
+++ JAVA/GestionPlanning/src/ihm/AccueilController.java
@@ -0,0 +1,24 @@
+package ihm;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AccueilController {
+
+    @FXML
+    private Button connectionButton;
+
+    @FXML
+    private TextField login;
+
+    @FXML
+    private PasswordField password;
+
+    public void connection(MouseEvent mouseEvent) {
+
+    }
+}
