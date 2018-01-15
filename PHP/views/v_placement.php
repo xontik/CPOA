f342a490c288307f7a18d34f4151947e7cbe59d3
Emma Glasson <ma.glasson@free.fr>
2018-01-15 17:21:15 +0100

    modification vue placement

--- PHP/views/v_placement.php
+++ PHP/views/v_placement.php
@@ -7,54 +7,39 @@
 
 <form action="index.php?page=achat" method="post" class="col s12" style="margin-top:30px;">
 	<div class="row">
-	<div class="col s6">
-		<div class="input-field" >
-			<select name="bloc" required>
-				<option value="" disabled selected>Choisissez un bloc :</option>
-				<?php
-				foreach($emplacements as $emplacement) {
-					echo '<option value="'.$emplacement->getBloc().'">'.$emplacement->getBloc().'</option>';
-				}
-				?>
-			</select>
-			<label>Bloc :</label>
+		<div class="col s6 offset-s3">
+			<div class="input-field" >
+				<select name="bloc" required>
+					<option value="" disabled selected>Choisissez un bloc :</option>
+					<?php
+					foreach($emplacements as $emplacement) {
+						echo '<option value="'.$emplacement->getBloc().'">'.$emplacement->getBloc().'</option>';
+					}
+					?>
+				</select>
+				<label>Bloc :</label>
+			</div>
+			
+			<div class="input-field">
+			  <input id="nb_billet" name="nbPlace" type="number" min="1" max="10" class="validate" required>
+			  <label for="nb_billet">Nombre de place :</label>
+			</div>
+			
+			<div class="input-field">
+			  <input id="code_promo" name="codePromo" type="text" class="validate">
+			  <label for="code_promo">Code promo :</label>
+			</div>
+			
+			<div class="input-field">
+			  <input id="num_licencie" name="licence" type="text" class="validate">
+			  <label for="num_licencie">Numéro de licence :</label>
+			</div>
+			
+			<button class="btn waves-effect waves-light" type="submit" name="achat"  style="margin-bottom:30px;">Finaliser l'achat
+				<i class="material-icons right">send</i>
+			</button>
 		</div>
-		<br>
-		<div class="input-field" >
-			<select name="place" required>
-			  <option value="" disabled selected>Choisissez une place :</option>
-			  <option value="1">51</option>
-			  <option value="2">55</option>
-			  <option value="3">56</option>
-			  <option value="4">57</option>
-			</select>
-			<label>Place :</label>
-		</div>
-	</div>
-	
-	<div class=" col s6">
-        <div class="input-field">
-          <input id="nb_billet" name="nbPlace" type="number" min="1" max="10" class="validate" required>
-          <label for="nb_billet">Nombre de place :</label>
-        </div>
-		<br>
-        <div class="input-field">
-          <input id="code_promo" name="codePromo" type="text" class="validate">
-          <label for="code_promo">Code promo :</label>
-        </div>
-		<br>
-        <div class="input-field">
-          <input id="num_licencie" name="licence" type="text" class="validate">
-          <label for="num_licencie">Numéro de licence :</label>
-        </div>
-    </div>
-	
-	
-	<button class="btn waves-effect waves-light" type="submit" name="achat"  style="margin-bottom:30px;">Finaliser l'achat
-		<i class="material-icons right">send</i>
-	</button>
-</div>
-	
+	</div>	
 </form>
 <!--  Fin de la page -->
 
