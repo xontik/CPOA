69aacd638941a60383621f81f1efe6de5c1c184e
Emma Glasson <ma.glasson@free.fr>
2018-01-16 10:49:47 +0100

    correction fautes orthographe

--- PHP/views/v_accueil.php
+++ PHP/views/v_accueil.php
@@ -1,17 +1,3 @@
-<?php
-/*
- * DS PHP
- * Vue page index - page d'accueil
- *
- * Copyright 2016, Eric Dufour
- * http://techfacile.fr
- *
- * Licensed under the MIT license:
- * http://www.opensource.org/licenses/MIT
- *
- */
-//  En tête de page
-?>
 <?php require_once(PATH_VIEWS.'header.php');?>
 
 
---
+++ PHP/views/v_contact.php
@@ -0,0 +1,36 @@
+<?php require_once(PATH_VIEWS.'header.php');?>
+
+<!--  Début de la page -->
+<form action="#" method="post" >
+<div class="row"> 
+	<div class="col s6 offset-s3">
+		<h1>Nous contacter</h1>
+		
+		<div class="input-field col s6">
+          <input  id="first_name" type="text" class="validate">
+          <label for="first_name">Nom</label>
+        </div>
+        <div class="input-field col s6">
+          <input id="last_name" type="text" class="validate">
+          <label for="last_name">Prénom</label>
+        </div>
+		 <div class="input-field col s12">
+          <input id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+		<div class="input-field col s12">
+          <textarea id="textarea1" class="materialize-textarea"></textarea>
+          <label for="textarea1">Quelle est votre question ? </label>
+        </div>
+		<div class="offset-s3">
+			<button class="btn waves-effect waves-light" type="submit" name="achat"  style="margin-bottom:30px;">Envoyer
+				<i class="material-icons right">send</i>
+			</button>
+		</div>
+	</div>
+</div>
+</form>
+<!--  Fin de la page -->
+
+<!--  Pied de page -->
+<?php require_once(PATH_VIEWS.'footer.php');
